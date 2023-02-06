ef6a70e7a854c38894c64f19dfd368bad421e206
Harsh Vashishtha <96041137+vashishtharsh@users.noreply.github.com>
2023-02-06 12:57:08 +0530

    Create Associativity.java

---
+++ Associativity.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Associativity {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /*
+        int x,y;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        int z = x*y/2;
+        System.out.println(z);
+        int a,b,c;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        int k = (b*b-(4*a*c))/(2*a);
+        System.out.println(k);
+        int v,u;
+        v = sc.nextInt();
+        u = sc.nextInt();
+        int w = v*v -u*u;
+        System.out.println(w);
+        int d,e,f;
+        d = sc.nextInt();
+        e = sc.nextInt();
+        f = sc.nextInt();
+        int g = d*e-f;
+        System.out.println(g);*/
+
+        int h = 4+5*8-3%2;
+        System.out.println(h);
+    }
+}
