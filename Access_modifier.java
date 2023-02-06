dbcf3881415e2a2db8c1682d43b87c92d41a65de
Harsh Vashishtha <96041137+vashishtharsh@users.noreply.github.com>
2023-02-06 12:55:58 +0530

    Create Access_modifier.java

---
+++ Access_modifier.java
@@ -0,0 +1,28 @@
+class MyEmployee{
+    private int id;
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+    public void setId(int i){
+        this.id = i;
+    }
+    public int getId(){
+        return id;
+    }
+}
+public class Access_modifier {
+    public static void main(String[] args) {
+        MyEmployee person = new MyEmployee();
+        // person.id = 45;
+        // person.name = "Priyanshu Baral"; --> Throws an error due to private access modifier
+        person.setName("Priyanshu Baral");
+        System.out.println(person.getName());
+        person.setId(1001);
+        System.out.println(person.getId());
+    }
+}
