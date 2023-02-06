49cae5b8825bc0bf45079f70f378c26b759d380c
Harsh Vashishtha <96041137+vashishtharsh@users.noreply.github.com>
2023-02-06 12:55:27 +0530

    Create Abstract_Class_Method.java

---
+++ Abstract_Class_Method.java
@@ -0,0 +1,25 @@
+abstract class Bank{
+    abstract int getRateOfInterest();
+}
+class SBI extends Bank{
+    @Override
+    int getRateOfInterest(){
+        return 7;
+    }
+}
+class PNB extends Bank{
+    @Override
+    int getRateOfInterest(){
+        return 8;
+    }
+}
+
+public class Abstract_Class_Method{
+    public static void main(String[] args){
+        Bank b;
+        b=new SBI();
+        System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");
+        b=new PNB();
+        System.out.println("Rate of Interest is: "+b.getRateOfInterest()+" %");
+    }
+}
