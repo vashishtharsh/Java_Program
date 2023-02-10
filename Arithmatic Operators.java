720e0cfcf81c1bdf25d44360b299cc32b75695fa
Harsh Vashishtha <96041137+vashishtharsh@users.noreply.github.com>
2023-02-10 13:12:21 +0530

    Create Arithmatic Operators.java

---
+++ Arithmatic Operators.java
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+var a =parseInt(prompt ("enter tha value a "));
+var b =parseInt(prompt ("enter tha value b"));
+var sub =a-b;
+var power =a**b;
+var multiply =a*b;
+var add =a+b;
+var divide =a/b;
+
+alert("value of a is "+a+" value of b is "+b );
+alert ("value of adding"+a+ "and "+b+" is"+ add );
+alert ("value of subtract "+a+ "and "+b+" is"+ sub );
+alert ("value of multiply"+a+ "and "+b+" is"+ multiply );
+alert ("value of divide"+a+ "and "+b+" is"+ divide  );
+alert ("value of power"+a+ "and "+b+" is"+ power);
+
+
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
