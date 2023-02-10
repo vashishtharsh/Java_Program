d957518d80992c9ddccc9f827e56f11588664751
Harsh Vashishtha <96041137+vashishtharsh@users.noreply.github.com>
2023-02-10 13:22:41 +0530

    Update Constructor.java

--- Constructor.java
+++ Constructor.java
@@ -3,15 +3,15 @@ class MyMainEmployee{
     private String name;
 
     public MyMainEmployee(){
-        id = 1003;
-        name = "Ashish Mishra";
+        id = 103;
+        name = "Aman Sharma";
     }
     public MyMainEmployee(String myName, int myId){
         id = myId;
         name = myName;
     }
     public MyMainEmployee(String myName){
-        id = 1002;
+        id = 102;
         name = myName;
     }
     public String getName(){
@@ -27,12 +27,12 @@ class MyMainEmployee{
 
 public class Constructor {
     public static void main(String[] args) {
-        MyMainEmployee priyanshu = new MyMainEmployee("Priyanshu Baral", 1001);
-        MyMainEmployee priyanshu1 = new MyMainEmployee("Abhishek Mishra");
-        MyMainEmployee priyanshu2 = new MyMainEmployee();
-        //priyanshu.setName("Priyanshu Baral");   --> This statement we get as output if we uncomment it.
-        //priyanshu.setId(34);
-        System.out.println(priyanshu.getId());
-        System.out.println(priyanshu.getName());
+        MyMainEmployee harsh = new MyMainEmployee("Harsh Vashishtha", 101);
+        MyMainEmployee harsh1 = new MyMainEmployee("Aman Sharma");
+        MyMainEmployee harsh2 = new MyMainEmployee();
+        //harsh.setName("Harsh Vashishtha");   --> This statement we get as output if we uncomment it.
+        //harsh.setId(34);
+        System.out.println(harsh.getId());
+        System.out.println(harsh.getName());
     }
 }
